659b68141e762c23bf8438f9e52206521ef4b362
Gabriel <ggauto994@gmail.com>
2022-05-23 19:21:35 -0400

    a medias ya el contact

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,60 @@
 import React from "react";
+import "../css/form.css";
 
 export default function Form() {
   return (
-    <section id="contact">
-      <form method="post" action="">
-        <input type="text" name="name" id="name" placeholder="Nombre" />
-        <input type="email" name="email" id="email" placeholder="Email" />
-        <input type="text" name="subject" id="subject" placeholder="Asunto" />
-        <input type="text" name="message" id="message" placeholder="Mensaje" />
-        <button type="submit" value="Send Mesagge">
-          Enviar
-        </button>
-        <button type="reset" value="Clear Form">
-          Limpiar
-        </button>
+    <section id="contact" className="contact">
+      <form method="post" action="" className="form-contact">
+        <div>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Nombre"
+            className="input-type1"
+          />
+        </div>
+
+        <div>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            className="input-type1"
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            name="subject"
+            id="subject"
+            placeholder="Asunto"
+            className="input-type2"
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            name="message"
+            id="message"
+            placeholder="Mensaje"
+          />
+        </div>
+
+        <div>
+          <button type="submit" value="Send Mesagge">
+            Enviar
+          </button>
+        </div>
+
+        <div>
+          <button type="reset" value="Clear Form">
+            Limpiar
+          </button>
+        </div>
       </form>
     </section>
   );
